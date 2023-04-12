f7fc580a00a13a394611ec13e18a5f2dec13d53d
Supper-Smille <130517074+Supper-Smille@users.noreply.github.com>
2023-04-12 16:55:38 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,22 +1,54 @@
-from setuptools import setup, find_packages
+# Author: Supper-Smille <ak2004@support.email.ne.jp>
+# Copyright (c) 2023 Supper-Smille
+# License: MIT License
+
+
+from setuptools import setup
+import MyJpHolidays
+
 
 DESCRIPTION = "MyJpHolidays: You get a combined list of Japan-Holidays. You can customize orignal holidays used ini-file."
 NAME = 'MyJpHolidays'
 AUTHOR = 'Supper-Smille'
 AUTHOR_EMAIL = 'ak2004@support.email.ne.jp'
-URL = 'https://github.com/Supper-Smille/MyJpHolidays20230412.git'
+URL = 'https://github.com/Supper-Smille/MyJpHolidays20230412'
 LICENSE = 'MIT License'
-DOWNLOAD_URL = 'https://github.com/Supper-Smille/MyJpHolidays20230412.git'
-VERSION = '0.8.0'
+DOWNLOAD_URL = 'https://github.com/Supper-Smille/MyJpHolidays20230412'
+VERSION = MyJpHolidays.__version__
 PYTHON_REQUIRES = ">=3.8.10"
+
 
 INSTALL_REQUIRES = [
     'numpy >=1.23.1', 
     're>=2.2.1'
 ]
 
-setup(
-    name='MyJpHolidays',
-    version='0.8.0',
-    packages=find_packages()
-)
+
+PACKAGES = [
+    'MyJpHolidays'
+]
+
+
+setup(name=NAME,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer=AUTHOR,
+      maintainer_email=AUTHOR_EMAIL,
+      description=DESCRIPTION,
+      
+      #long_description=long_description,
+      
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      
+      # extras_require=EXTRAS_REQUIRE,
+      
+      packages=PACKAGES,
+      
+      #classifiers=CLASSIFIERS
+    )
+
